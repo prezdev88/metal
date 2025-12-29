ebdb8808b10d05469a459d32f7d5f907b2f02a8e
Patricio Pérez Pinto <patodeath@gmail.com>
2025-12-29 01:46:12 -0300

    init: first commit

---
+++ src/main/java/com/not/vibe/metal/MetalApplication.java
@@ -0,0 +1,13 @@
+package com.not.vibe.metal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/not/vibe/metal/MetalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.not.vibe.metal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
